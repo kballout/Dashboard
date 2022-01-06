b7080ea51ad5d9289617350248c41b31c0e171b6
kballout <54900156+kballout@users.noreply.github.com>
2022-01-06 15:50:51 -0500

    Improved leaderboards

--- dash/routes/dashboard-routes.js
+++ dash/routes/dashboard-routes.js
@@ -363,62 +363,11 @@ router.get('/servers/:id/stats', validateGuild, async (req, res) => {
         nextUser['untilNextLevel'] = 5 * Math.pow(nextUser['level'], 2) + 50 * nextUser['level'] + 100,
         users.push(nextUser);
     }
-
-    //sort
-    
-    var usersByCurrentPoints = JSON.parse(JSON.stringify(users)); 
-    var usersByTotalPoints = JSON.parse(JSON.stringify(users));
-    var usersByMonthlyPoints = JSON.parse(JSON.stringify(users));
-    var usersByTotalExchange = JSON.parse(JSON.stringify(users));
-    var usersByMonthlyExchange = JSON.parse(JSON.stringify(users));
-    var usersByHighestStreak = JSON.parse(JSON.stringify(users));
-    var usersByAttendance = JSON.parse(JSON.stringify(users));
-    var usersByLevel = JSON.parse(JSON.stringify(users));
-    var usersByMessages = JSON.parse(JSON.stringify(users));
-    
-    
-
-    usersByCurrentPoints.sort(function (a, b){
-        return b.currentPoints - a.currentPoints;
-    })
-    usersByTotalPoints.sort(function (a, b){
-        return b.totalPoints - a.totalPoints;
-    })
-    usersByMonthlyPoints.sort(function (a, b){
-        return b.monthlyPoints - a.monthlyPoints;
-    })
-    usersByTotalExchange.sort(function (a, b){
-        return b.totalExchange - a.totalExchange;
-    })
-    usersByMonthlyExchange.sort(function (a, b){
-        return b.monthlyExchange - a.monthlyExchange;
-    })
-    usersByHighestStreak.sort(function (a, b){
-        return b.highestStreak - a.highestStreak;
-    })
-    usersByAttendance.sort(function (a, b){
-        return b.attendance - a.attendance;
-    })
-    usersByMessages.sort(function (a, b){
-        return b.messages - a.messages;
-    })
-    usersByLevel.sort(function (a, b){
-        return b.level - a.level || b.xp - a.xp;
-    })
-
     
     res.render('dashboard/modules/leaderboards', {
         guildExists,
         loggedIn,
-        usersByTotalPoints,
-        usersByCurrentPoints,
-        usersByMonthlyPoints,
-        usersByTotalExchange,
-        usersByMonthlyExchange,
-        usersByHighestStreak,
-        usersByAttendance,
-        usersByMessages,
-        usersByLevel,
+        users,
         subtitle: 'Leaderboards'
     })
     
